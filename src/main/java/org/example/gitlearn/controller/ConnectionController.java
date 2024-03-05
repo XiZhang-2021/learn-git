3394cee1be8fdc4f649657f7e63e8f1a890d07da
xizhang <xizhang4949011@outlook.com>
2024-03-05 10:55:20 -0800

    first commit

---
+++ src/main/java/org/example/gitlearn/controller/ConnectionController.java
@@ -0,0 +1,23 @@
+package org.example.gitlearn.controller;
+
+import jdk.jshell.Snippet;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.net.ssl.SSLEngineResult;
+
+@RestController
+@RequestMapping("/connection")
+public class ConnectionController {
+    @GetMapping("/v1")
+    public ResponseEntity getV1(){
+        System.out.println("this is v1 version");
+
+        return ResponseEntity.ok("success, this is v1 version!");
+    }
+}
---
+++ src/test/java/org/example/gitlearn/GitLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gitlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
