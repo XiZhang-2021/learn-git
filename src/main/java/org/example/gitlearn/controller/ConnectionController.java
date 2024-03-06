953d18e1d57e71a5eaf590835b7e386091285fe0
xizhang <xizhang4949011@outlook.com>
2024-03-05 18:00:06 -0800

    resolve confilct third

--- src/main/java/org/example/gitlearn/controller/ConnectionController.java
+++ src/main/java/org/example/gitlearn/controller/ConnectionController.java
@@ -53,6 +53,8 @@ public class ConnectionController {
 
         System.out.println("this is change in feature2 branch");
 
+        System.out.println("this is the second change in feature2 branch");
+
         return ResponseEntity.ok("success, this is v6 version!");
     }
 
